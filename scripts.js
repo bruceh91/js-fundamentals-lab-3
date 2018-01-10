b10f66694a66cdc876ba24585fb6d4bc7ab5008f
Bruce Harris <bruceharris91@gmail.com>
2018-01-10 17:01:39 -0600

    add lab 3

---
+++ scripts.js
@@ -0,0 +1,26 @@
+
+let friendArr = ["Andy", "Sari", "Ernest", "Travis", "Dante"]
+let count = 99;
+
+
+
+function singSong (friendArr) {
+    let name = "";
+    let z = 0;
+    for (let i = 0; i < friendArr.length; i++) {
+        name = friendArr[i];
+        tempVal = name.toUpperCase();
+        console.log(tempVal + ":");
+        for (let j = count; j > 0; j--)
+            
+            if (j>1) {
+                z = j - 1
+                console.log(name + " saw " + j + " aliens in the sky, " + name + " saw " + j + " aliens. Shoot one down. Take to a secret base in Nevada, and deny it's existance. " + z + " aliens in the sky.");
+            } else {
+                console.log(name + " saw " + j + " alien in the sky. " + name + " saw " + j + " alien. Shoot one down. Take to a secret base in Nevada, and deny it's existance. No more aliens in the sky.");
+            }
+    }
+}
+
+singSong (friendArr);
+
