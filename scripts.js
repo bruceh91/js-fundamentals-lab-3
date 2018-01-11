eaccdc3f2673e112799dab751b2d286fd6845675
Bruce Harris <bruceharris91@gmail.com>
2018-01-11 10:37:43 -0600

    adding lab

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-let friendArr = ["Andy", "Sari", "Ernest", "Travis", "Dante"]
+let friendArr = ["Andy", "Sari", "Ernest", "Travis", "Dante"];
 let count = 99;
 
 
@@ -15,9 +15,9 @@ function singSong (friendArr) {
             
             if (j>1) {
                 z = j - 1
-                console.log(name + " saw " + j + " aliens in the sky, " + name + " saw " + j + " aliens. Shoot one down. Take to a secret base in Nevada, and deny it's existance. " + z + " aliens in the sky.");
+                console.log(name + " saw " + j + " aliens in the sky, " + name + " saw " + j + " aliens. Shoot one down. Take one to a secret base in Nevada, and deny it's existance. " + z + " aliens in the sky.");
             } else {
-                console.log(name + " saw " + j + " alien in the sky. " + name + " saw " + j + " alien. Shoot one down. Take to a secret base in Nevada, and deny it's existance. No more aliens in the sky.");
+                console.log(name + " saw " + j + " alien in the sky. " + name + " saw " + j + " alien. Shoot one down. Take it to a secret base in Nevada, and deny it's existance. No more aliens in the sky.");
             }
     }
 }
